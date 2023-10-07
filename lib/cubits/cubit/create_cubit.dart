fa5b69cab4b94da9658d3804fb11de29ecbf9822
elesawy <elesawy928@gmail.com>
2023-10-08 00:53:28 +0300

    first commit

---
+++ lib/cubits/cubit/create_cubit.dart
@@ -0,0 +1,21 @@
+// import 'dart:convert';
+
+// import 'package:bloc/bloc.dart';
+// import 'package:meta/meta.dart';
+// import 'package:widget_example/main.dart';
+// import 'package:http/http.dart'as http;
+// part 'create_state.dart';
+
+// class CreateCubit extends Cubit<CreateState> {
+//   CreateCubit() : super(CreateInitial());
+
+// void createAlbum(String title) async {
+//   emit( CreateLoading());
+//   try {
+//   Album album = await createAlbum(title);
+// } on Exception catch (e) {
+//   // TODO
+// }
+  
+// }
+// }
---
+++ lib/cubits/cubit/create_state.dart
@@ -0,0 +1,15 @@
+// part of 'create_cubit.dart';
+
+// @immutable
+// abstract class CreateState {}
+
+//  class CreateInitial extends CreateState {}
+//  class CreateFailure extends CreateState {
+ 
+//  }
+//  class CreateLoading extends CreateState {}
+//  class CreateSuccess extends CreateState {
+//   final Album album;
+
+//   CreateSuccess({required this.album});
+//  }
